bb160526ad0982fb7b190f634a83603746a45b7a
K-ennethA <43216812+K-ennethA@users.noreply.github.com>
2018-09-29 13:56:37 -0700

    Add files via upload

--- BrowserHistory.h
+++ BrowserHistory.h
@@ -10,42 +10,101 @@ using namespace std;
 
 class BrowserHistory {
 public:
-    BrowserHistory();
+	BrowserHistory();
 
-    void visitSite(Webpage newSite);
-    string back();
-    string forward();
+	void visitSite(Webpage newSite);
+	string back();
+	string forward();
 
-    void readHistory(string fileName);
+	void readHistory(string fileName);
 
-    string getUrl();
-    size_t getNavSize();
-    list<Webpage> getSitesVisited();
+	string getUrl();
+	size_t getNavSize();
+	list<Webpage> getSitesVisited();
 
 private:
-    list<Webpage> navHistory;
-    list<Webpage>::iterator navPos;
-    list<Webpage> sitesVisited;
-    int numSites;
+	list<Webpage> navHistory;
+	list<Webpage>::iterator navPos;
+	list<Webpage> sitesVisited;
+	int numSites;
 };
 
 void BrowserHistory::readHistory(string fileName) {
-    string temp;
-    int newTime;
-    Webpage newSite;
-    ifstream inFile(fileName.c_str());
+	string temp;
+	int newTime;
+	Webpage newSite;
+	ifstream inFile(fileName.c_str());
 
-    while(inFile >> temp) {
-        if(temp == "new") {
-            inFile >> temp >> newTime;
-            newSite = Webpage(temp, time_t(newTime));
-            visitSite(newSite);
-        } else if(temp == "back") {
-            back();
-        } else if(temp == "forward") {
-            forward();
-        } else {
-            throw logic_error("invalid command");
-        }
-    }
+	while (inFile >> temp) {
+		if (temp == "new") {
+			inFile >> temp >> newTime;
+			newSite = Webpage(temp, time_t(newTime));
+			visitSite(newSite);
+		}
+		else if (temp == "back") {
+			back();
+		}
+		else if (temp == "forward") {
+			forward();
+		}
+		else {
+			throw logic_error("invalid command");
+		}
+	}
 }
+
+BrowserHistory::BrowserHistory(){
+	navHistory = {};
+	navPos = {};
+	sitesVisited = {};
+	numSites = 0;
+}
+
+void BrowserHistory::visitSite(Webpage newsite) {
+	
+	if (&navPos != nullptr) {
+		for (navPos = navHistory.begin(); navPos != navHistory.end(); navPos++) {
+			navHistory.push_back(newsite);
+			navHistory.pop_back();
+		}
+	}
+	else
+		navHistory.push_back(newsite);
+}
+
+string BrowserHistory::back() {
+	if (navPos != navHistory.begin()) {
+		navPos--;
+		return navPos->getUrl();
+	}
+	else
+		throw invalid_argument("You cannot go back");
+}
+string BrowserHistory::forward() {
+	if (navPos != navHistory.end()) {
+		navPos++;
+		return navPos->getUrl();
+
+		}
+	else 
+		throw invalid_argument("You cannot go forward");
+}
+
+ string BrowserHistory::getUrl()
+{
+	
+}
+
+size_t BrowserHistory::getNavSize(){
+	return navHistory.size();
+}
+
+list<Webpage> BrowserHistory::getSitesVisited() {
+
+}
+
+
+
+
+
+
