7b2672266fa8b65db1d8508dabcdc1c4212159e1
K-ennethA <43216812+K-ennethA@users.noreply.github.com>
2018-10-04 15:41:02 -0700

    Add files via upload

--- BrowserHistory.h
+++ BrowserHistory.h
@@ -65,9 +65,9 @@ void BrowserHistory::visitSite(Webpage newsite) {
 	if ( numSites == 0) {
 		navPos = navHistory.begin();
 	}
-	else if (navPos != navHistory.end()) {
-		navHistory.erase(navPos++, navHistory.end());
-	}
+	//else if (navPos != navHistory.end()) {
+	//	navHistory.erase(++navPos, navHistory.end());
+	//}
 	navHistory.push_back(newsite);
 	++navPos;
 	++numSites;
