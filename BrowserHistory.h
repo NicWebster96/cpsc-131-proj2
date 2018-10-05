daee2a95bc21b71327e972db89423d4223b26e88
K-ennethA <43216812+K-ennethA@users.noreply.github.com>
2018-10-05 12:32:18 -0700

    Add files via upload

--- BrowserHistory.h
+++ BrowserHistory.h
@@ -53,38 +53,38 @@ void BrowserHistory::readHistory(string fileName) {
 	}
 }
 
-BrowserHistory::BrowserHistory(){
+BrowserHistory::BrowserHistory(){         //initliazes all to null
 	navHistory = {};
 	navPos = {};
 	sitesVisited = {};
 	numSites = 0;
 }
 
-void BrowserHistory::visitSite(Webpage newsite) {
-	sitesVisited.push_back(newsite);	
-	if ( numSites == 0) {
+void BrowserHistory::visitSite(Webpage newsite) {   
+	sitesVisited.push_back(newsite);	//pushes back because sitesVisited contains all sites
+	if ( numSites == 0) {                 //checks to see if empty to set iterator to the beginning 
 		navPos = navHistory.begin();
-	}
-	else if ( navPos != --navHistory.end()){
-		list<Webpage>::iterator itr;
+	}											 // when make a newsite and not at end and there are sites after we erase them
+	else if ( navPos != --navHistory.end()){     //else if not at the last element we make a new iterator that holds our current position 
+		list<Webpage>::iterator itr;             //then increments it and erases the temp iterator and the last element
 		itr = navPos;
 		itr++;
 		navHistory.erase(itr, navHistory.end());
 	}
-	navHistory.push_back(newsite);
-	++navPos;
-	++numSites;
+	navHistory.push_back(newsite);          //pushes newsite onto NavHistory
+	++navPos;								//increments our iterator position
+	++numSites;								//increments the number of Sites
 }
 
-string BrowserHistory::back() {
-	if (navPos != navHistory.begin()) {
-		--numSites;
-		--navPos;
+string BrowserHistory::back() {             //allows our iterator to move backwards in the list 
+	if (navPos != navHistory.begin()) {     //decrements them 
+		--numSites;                         //checks to see if at beginning because we can't move back if already at beginning
+		--navPos;  
 	}
-	else throw invalid_argument("You cannot go back");
+	else throw invalid_argument("You cannot go back");  
 	return navPos->getUrl();
 }
-string BrowserHistory::forward() {
+string BrowserHistory::forward() {          //moves iterator forward, checks to see if at end because can't move forward if at end
 	if (navPos != --navHistory.end()){
 		++numSites;
 		++navPos;
@@ -94,18 +94,18 @@ string BrowserHistory::forward() {
 
 }
 
- string BrowserHistory::getUrl(){
+ string BrowserHistory::getUrl(){                      //checks to see if we have visited any Sites, returns getUrl
 	 if (numSites == 0) {
 		 throw invalid_argument("You have not visited any sites");
 	 }
 	return navPos->getUrl();
 }
 
-size_t BrowserHistory::getNavSize(){
+size_t BrowserHistory::getNavSize(){            //returns the size of Nav History
 	return navHistory.size();
 }
 
-list<Webpage> BrowserHistory::getSitesVisited() {
+list<Webpage> BrowserHistory::getSitesVisited() {       //returns the total number of sitesvisited
 	return sitesVisited;
 }
 
